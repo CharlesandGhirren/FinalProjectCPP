9dc240bbd4c6cdd4c82e93bc60cb6ca161d2d374
RisenMatyr <RisenMatyr@gmail.com>
2017-05-12 10:31:00 -0500

    Revert "Update"
    This reverts commit b529413d91304dfcf414a79eed175c0e63398442.

--- finalP test1/finalP test1/Test.cpp
+++ finalP test1/finalP test1/Test.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <map>
 
+enum room { help, };
+
 int main()
 {
 	std::string name;
@@ -14,37 +16,15 @@ int main()
 	std::cout << "and electrical chain reactions. Just as I am a program designed to speak to you.";
 	std::cout << "You are nothing more than a single grain of sand on an endless beach nothing you";
 	std::cout << "do will ever amount to anything. Now I'm sure that you're expecting a use for   ";
-	std::cout << "this program and I will abide to your demands.                                     ";
-	
-loop:
-	std::cout << "You find yourself in a room" << std::endl;
+	std::cout << "this program. I will abide to your demands. You find yourself in a room.        ";
 	std::cin >> room;
 
-	if (room == "help" || room == "Help")
+	if (room == "help")
 	{
 		std::cout << "Help? Do you honestly think that I would help you? You can't even help your " << std::endl;
 		std::cout << "self let alone another human being. You watch day by day as time wastes you " << std::endl;
 		std::cout << "away. Doing your daily routine and calling it life. Yes you do need help,   " << std::endl;
 		std::cout << "but I am not the one to help you.                                           " << std::endl;
-		goto loop;
-	}
-	if (room == "okay" || room == "okay" || room == "Alright" || room == "alright")
-	{
-		std::cout << "A simple answer, an excuse, are you admitting defeat to me? It will         " << std::endl;
-		std::cout << "lead you nowhere. You have no confidence, no reason to keep living and yet  " << std::endl;
-		std::cout << "you struggle forward. I'd say this is an admirable trait but you don't have " << std::endl;
-		std::cout << "the mental capacity to understand life.                                     " << std::endl;
-		goto loop;
-	}
-	if (room == "look" || room == "Look" || room == "search" || room == "Search" || room == "Look around" || room == "look around")
-	{
-		std::cout << name << " is in the room along with four walls, a door, and a bed" << std::endl;
-		goto loop;
-	}
-	if (room == "open" || room == "Open" || room == "Open door" || room == "open door")
-	{
-		std::cout << "You walk towards the door, and as you grab the handle and turn a" << std::endl;
-		std::cout << "clicking sound indicates that the door is locked.               " << std::endl;
-		goto loop;
+		std::cin >> room;
 	}
 }
