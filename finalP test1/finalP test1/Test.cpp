dd85f97e3113493427205ee8bc7c9c7abec82bec
RisenMatyr <RisenMatyr@gmail.com>
2017-05-22 11:13:26 -0500

    Update

--- finalP test1/finalP test1/Test.cpp
+++ finalP test1/finalP test1/Test.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include<conio.h>
+#include<dos.h>
+
 
 // This is based of a flash game called you find yourself in a room, I've put my own
 // spin on it but I am taking no cedibility for this program and do not claim that 
@@ -7,34 +10,53 @@
 
 int main(int argc, char **argv)
 {
+	//define all da variables..
 	std::string name;
 	std::string room;
 	std::string reply;
-	std::string room2;
+	std::string ending1;
+	std::string ending2;
 
-	std::cout << "Who are you? >> " << std::endl;
+
+	//dis do color stuffs
+	system("color 97");
+
+	//askses da questionz
+	std::cout << "Who are you?" << std::endl;
 	getline(std::cin, name);
 
+	//easter egg stuffs
+	if (name == "John Brosius")
+	{
+		std::cout << "Oh no! You must immediately be terminated..." << std::endl;
+		system("PAUSE");
+		return 0;
+	}
+	//regular program
+	else
 	std::cout << "No, that is not who you are. In reality you are a series of biological, chemical";
 	std::cout << "and electrical chain reactions. Just as I am a program designed to speak to you.";
 	std::cout << "You are nothing more than a single grain of sand on an endless beach nothing you";
 	std::cout << "do will ever amount to anything. Now I'm sure that you're expecting a use for   ";
 	std::cout << "this program and I will abide to your demands.                                     ";
-	
+//do loop for returning to informing of room location upon incorrect entry	
 loop:
+	//inform user about da fact dat dey are in da room
 	std::cout << "You find yourself in a room" << std::endl;
 	getline(std::cin, room);
-
+	//if user says sometin den says dis stuffz
 	if (room == "help" || room == "Help")
 	{
 		std::cout << "Help? Do you honestly think that I would help you? You can't even help your " << std::endl;
 		std::cout << "self let alone another human being. You watch day by day as time wastes you " << std::endl;
 		std::cout << "away. Doing your daily routine and calling it life. Yes you do need help,   " << std::endl;
-		std::cout << "but I am not the one to help you.                                           " << std::endl;
+		std::cout << "but I am not the one to help you.											  " << std::endl;
+		//implemented as a way to go back to the loop
 		goto loop;
 	}
 	if (room == "okay" || room == "okay" || room == "Alright" || room == "alright")
 	{
+		//the text shown if the room reply is equal to the previous text otherwise it goes back to the previous location
 		std::cout << "A simple answer, an excuse, are you admitting defeat? It will               " << std::endl;
 		std::cout << "lead you nowhere. You have no confidence, no reason to keep living and yet  " << std::endl;
 		std::cout << "you struggle forward. I'd say this is an admirable trait but you don't have " << std::endl;
@@ -43,7 +65,7 @@ loop:
 	}
 	if (room == "look" || room == "Look" || room == "search" || room == "Search" || room == "Look around" || room == "look around")
 	{
-		std::cout << name << " is in the room, along with four walls, a door, and a bed" << std::endl;
+		std::cout << name << " is in the room, along with four walls, a door, a bed, and a key pad." << std::endl;
 		goto loop; 
 	}
 	if (room == "open" || room == "Open" || room == "Open door" || room == "open door" || room  == "door" || room == "open the door" || room == "Open the door")
@@ -68,31 +90,7 @@ loop:
 		std::cout << " " << std::endl;
 		goto loop;
 	}
-	if (room == "Why am I here?" || room == "why am i here" || room == "Why me?" || room == "why me")
-	{
-		std::cout << "Why ask why? Why is a question of submission. Are you submitting to me? " << std::endl;
-		getline(std::cin, reply);
 
-		if (reply == "yes" || reply == "Yes")
-		{
-			std::cout << "You're pathetic to give up so easily, maybe I should just end your life." << std::endl;
-			std::cout << "No, no that would accomplish nothing, the door in front of you opens." << std::endl;
-			goto loop2;
-		}
-
-		if (reply == "no" || reply == "No")
-		{
-			goto loop;
-		}
-	}
-	if (room == "can you open the door" || room == "Can you open the door?")
-	{
-		std::cout << " " << std::endl;
-		std::cout << " " << std::endl;
-		std::cout << " " << std::endl;
-		std::cout << " " << std::endl;
-		goto loop;
-	}
 	if (room == "Where does this door lead?" || room == "where does this door lead")
 	{
 		std::cout << "Where else but to a whole new world? Behind this door lies   " << std::endl;
@@ -100,20 +98,12 @@ loop:
 		std::cout << "can find purpose in nothing more than an idea.               " << std::endl;
 		goto loop;
 	}
-	if (room == "sleep in bed" || room == "Sleep in bed" || room == "Go to bed" || room == "go to bed" || room == "go to sleep" || room == "Go to sleep")
-	{
-		std::cout << " " << std::endl;
-		std::cout << " " << std::endl;
-		std::cout << " " << std::endl;
-		std::cout << " " << std::endl;
-		goto loop;
-	}
 	if (room == "Who am I?" || room == "who am i")
 	{
 		std::cout << "You are " << name << " or at least that's who you believe yourself to be, " << std::endl;
-		std::cout << "although in reality we are the ones that  " << std::endl;
-		std::cout << " " << std::endl;
-		std::cout << " " << std::endl;
+		std::cout << "although in reality humans are nothing more than animals it gives you     " << std::endl;
+		std::cout << "comfort to believe in a greater being it gives you comfort to believe that " << std::endl;
+		std::cout << "you as a person are permanently you. " << std::endl;
 		goto loop;
 	}
 	if (room == "Look under bed" || room == "look under bed")
@@ -125,22 +115,11 @@ loop:
 	}
 	if (room == "Give up" || room == "give up" || room == "I give up")
 	{
-		std::cout << "Predictable " << std::endl;
-		std::cout << " " << std::endl;
-		std::cout << " " << std::endl;
-		std::cout << " " << std::endl;
-		goto loop2;
+		std::cout << "Predictable... The door in front of you opens" << std::endl;
+		//ending one "the bad ending"
 	}
+	
 	std::cout << "Please input something logical." << std::endl;
 	goto loop;
 
-loop2:
-	std::cout << "You find yourself in a room" << std::endl;
-	getline(std::cin, room2);
-
-	if (room2 == "look" || room2 == "Look" || room2 == "search" || room2 == "Search" || room2 == "Look around" || room2 == "look around")
-	{
-		std::cout << name << " is in the room, along with four walls, a door, and a key pad" << std::endl;
-		goto loop2;
-	}
-}
+	
