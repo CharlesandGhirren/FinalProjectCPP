c6e08228012a659ac65178025a0e3a68b263b8b7
RisenMatyr <RisenMatyr@gmail.com>
2017-05-24 09:26:29 -0500

    Update

--- finalP test1/finalP test1/Test.cpp
+++ finalP test1/finalP test1/Test.cpp
@@ -2,12 +2,14 @@
 #include <string>
 #include<conio.h>
 #include<dos.h>
-
+#include <windows.h>
+#include <mmsystem.h>
 
 // This is based of a flash game called you find yourself in a room, I've put my own
 // spin on it but I am taking no cedibility for this program and do not claim that 
 // I came up with the idea
 
+int random(int ran);
 int main(int argc, char **argv)
 {
 	//define all da variables..
@@ -16,7 +18,7 @@ int main(int argc, char **argv)
 	std::string reply;
 	std::string ending1;
 	std::string ending2;
-
+	int number;
 
 	//dis do color stuffs
 	system("color 97");
@@ -34,12 +36,12 @@ int main(int argc, char **argv)
 	}
 	//regular program
 	else
-	std::cout << "No, that is not who you are. In reality you are a series of biological, chemical";
+		std::cout << "No, that is not who you are. In reality you are a series of biological, chemical";
 	std::cout << "and electrical chain reactions. Just as I am a program designed to speak to you.";
 	std::cout << "You are nothing more than a single grain of sand on an endless beach nothing you";
 	std::cout << "do will ever amount to anything. Now I'm sure that you're expecting a use for   ";
 	std::cout << "this program and I will abide to your demands.                                     ";
-//do loop for returning to informing of room location upon incorrect entry	
+	//do loop for returning to informing of room location upon incorrect entry	
 loop:
 	//inform user about da fact dat dey are in da room
 	std::cout << "You find yourself in a room" << std::endl;
@@ -66,9 +68,9 @@ loop:
 	if (room == "look" || room == "Look" || room == "search" || room == "Search" || room == "Look around" || room == "look around")
 	{
 		std::cout << name << " is in the room, along with four walls, a door, a bed, and a key pad." << std::endl;
-		goto loop; 
+		goto loop;
 	}
-	if (room == "open" || room == "Open" || room == "Open door" || room == "open door" || room  == "door" || room == "open the door" || room == "Open the door")
+	if (room == "open" || room == "Open" || room == "Open door" || room == "open door" || room == "door" || room == "open the door" || room == "Open the door")
 	{
 
 		std::cout << "You walk towards the door, and as you grab the handle and turn a" << std::endl;
@@ -86,8 +88,6 @@ loop:
 	{
 		std::cout << "You must find your own way out, perhaps it is not the room that you are " << std::endl;
 		std::cout << "escaping but you yourself.  " << std::endl;
-		std::cout << " " << std::endl;
-		std::cout << " " << std::endl;
 		goto loop;
 	}
 
@@ -108,6 +108,7 @@ loop:
 	}
 	if (room == "Look under bed" || room == "look under bed")
 	{
+		
 		std::cout << "Searching under the bed you find nothing, not that it would matter." << std::endl;
 		std::cout << "I highly doubt you would be able to figure out what you needed to do " << std::endl;
 		std::cout << "even if there was something here." << std::endl;
@@ -118,8 +119,47 @@ loop:
 		std::cout << "Predictable... The door in front of you opens" << std::endl;
 		//ending one "the bad ending"
 	}
-	
-	std::cout << "Please input something logical." << std::endl;
-	goto loop;
+	if (room == "look at pad lock" || room == "Look at pad lock" || room == "padlock")
+	{
+		std::cout << "The pad lock seems to require four numbers, the amount of possible" << std::endl;
+		std::cout << "combinations you could input would make figuring it out nearly " << std::endl;
+		std::cout << "impossible you would be stuck here till the end of your days.  " << std::endl;
+		goto loop;
+	}
+	if (room == "What are you?" || room == "what are you?" || "what are you" || room == "What are you")
+	{
+		std::cout << "I am nothing more than a program designed to enterain your simple" << std::endl;
+		std::cout << "mind created on may seventh for entertainment " << std::endl;
+		goto loop;
+	}
+	if (room == "enter numbers" || room == "put in numbers")
+	{
+		goto loop2;
+	}
+	else
+		std::cout << "Please input something logical." << std::endl;
+		goto loop;
 
 	
+loop2:
+	std::cout << "Enter numbers" << std::endl;
+	std::cin >> number;
+	if (number = 0507)
+	{
+		std::cout << "The day I was created, clever. The door opens, perhaps you " << std::endl;
+		std::cout << "have worth after all.";
+	}
+	else
+	{
+		std::cout << "That is not the code." << std::endl;
+		goto loop2;
+	}
+
+}
+int random (int ran){
+	ran = rand() % 10 + 1;
+
+}
+int movement (int random){
+	PlaySound(TEXT("footsteps.wav"), NULL, SND_ASYNC);
+}
